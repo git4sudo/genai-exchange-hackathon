5020eddf037337d0137eadd7a10326c6041a72fe
devil07625 <antivirus076@gmail.com>
2025-10-23 19:38:24 +0530

    with complete directory

---
+++ web/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/752a4bd6387d6ef7.js",
+    "static/chunks/7a4a1d89b022c184.js",
+    "static/chunks/c518a733bdcf3dee.js",
+    "static/chunks/turbopack-6ef6d91ad935870e.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
